d789981b1c594e7332dd1ac4535b52f788e85742
David Quang <tranngvietquang04@gmail.com>
2024-10-14 22:41:57 +0700

    create test asynchronous code

--- src/services/index.js
+++ src/services/index.js
@@ -4,4 +4,7 @@ export class PythagoreanService {
     static calculateHypotenuse(sideA, sideB) {
         return PythagoreanModel.calculateHypotenuse(sideA, sideB);
     }
+    static calculateHypotenuseAsync(sideA, sideB) {
+        return PythagoreanModel.calculateHypotenuseAsync(sideA, sideB);
+    }
 }
